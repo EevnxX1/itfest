f750a70311be0da01da5978edce6c2b58b519b94
EevnxX <miftamartin1@gmail.com>
2024-07-13 12:52:39 -0700

    di perbarui

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       colors: {
         color1: '#2FA8FF',
         color2: '#426394',
+        color3: '#263238',
+        color4: '#002547',
+      },
+      boxShadow: {
+        boxKompetisi: '9px 10px 7px 0px rgba(0, 0, 0, 0.3)',
       },
     },
   },
