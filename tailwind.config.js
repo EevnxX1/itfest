18fa83d0a6120e69113d2b4ae63290766a732c6b
EevnxX <miftamartin1@gmail.com>
2024-07-14 14:54:25 -0700

    Tinggal footer desain/code form google responsive animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,19 @@ module.exports = {
     extend: {
       fontFamily: {
         kanit: 'Kanit',
+        inknut: 'Inknut Antiqua',
+        itim: 'Itim',
       },
       colors: {
         color1: '#2FA8FF',
         color2: '#426394',
         color3: '#263238',
         color4: '#002547',
+        color5: '#597F92',
       },
       boxShadow: {
         boxKompetisi: '9px 10px 7px 0px rgba(0, 0, 0, 0.3)',
+        boxPendaftaran: '0px 0px 15px 0px #2FA8FF',
       },
     },
   },
